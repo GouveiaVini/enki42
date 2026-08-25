bb298bf6b477fa9e108cb97fac34bf29d62f0534
Víni <gouveiavinicius96@gmail.com>
2026-08-25 16:10:45 -0300

    refactor: simplify dongle status screen

--- config/boards/shields/enki42_minimal_display/custom_status_screen.c
+++ config/boards/shields/enki42_minimal_display/custom_status_screen.c
@@ -10,7 +10,6 @@
 #include "widgets/hid_indicators.h"
 #include "widgets/layer_status.h"
 #include "widgets/modifiers.h"
-#include "widgets/wpm_status.h"
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -22,7 +21,6 @@ static struct zmk_widget_modifiers modifiers_widget;
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
 static struct zmk_widget_hid_indicators hid_indicators_widget;
 #endif
-static struct zmk_widget_wpm_status wpm_status_widget;
 
 static lv_style_t global_style;
 static lv_style_t layer_style;
@@ -38,13 +36,15 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_t *layer = zmk_widget_layer_status_obj(&layer_status_widget);
     lv_style_init(&layer_style);
-    lv_style_set_text_font(&layer_style, &lv_font_montserrat_14);
+    lv_style_set_text_font(&layer_style, &lv_font_unscii_16);
     lv_obj_add_style(layer, &layer_style, LV_PART_MAIN);
     lv_obj_set_width(layer, 128);
     lv_obj_align(layer, LV_ALIGN_TOP_MID, 0, -2);
 
-    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_LEFT_MID, 0, 2);
+    zmk_widget_modifiers_init(&modifiers_widget, screen);
+    lv_obj_t *modifiers = zmk_widget_modifiers_obj(&modifiers_widget);
+    lv_obj_set_style_transform_zoom(modifiers, 320, LV_PART_MAIN);
+    lv_obj_align(modifiers, LV_ALIGN_LEFT_MID, 0, 2);
 
 #if IS_ENABLED(CONFIG_ZMK_BATTERY)
     zmk_widget_dongle_battery_status_init(&battery_status_widget, screen);
@@ -55,9 +55,6 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
     lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 #endif
-
-    zmk_widget_modifiers_init(&modifiers_widget, screen);
-    lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
 
     return screen;
 }
