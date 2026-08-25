92588c5fd9872596bd79358d72953b86d3a27ea3
Víni <gouveiavinicius96@gmail.com>
2026-08-25 15:57:31 -0300

    feat: add minimal dongle display

---
+++ config/boards/shields/enki42_minimal_display/custom_status_screen.c
@@ -0,0 +1,63 @@
+/*
+ * Minimal 128x64 dongle status screen for Enki42.
+ * Reuses the hardware-tested widgets from englmaxi/zmk-dongle-display v0.3.
+ */
+
+#include <lvgl.h>
+#include <zephyr/logging/log.h>
+
+#include "widgets/battery_status.h"
+#include "widgets/hid_indicators.h"
+#include "widgets/layer_status.h"
+#include "widgets/modifiers.h"
+#include "widgets/wpm_status.h"
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+static struct zmk_widget_layer_status layer_status_widget;
+#if IS_ENABLED(CONFIG_ZMK_BATTERY)
+static struct zmk_widget_dongle_battery_status battery_status_widget;
+#endif
+static struct zmk_widget_modifiers modifiers_widget;
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+static struct zmk_widget_hid_indicators hid_indicators_widget;
+#endif
+static struct zmk_widget_wpm_status wpm_status_widget;
+
+static lv_style_t global_style;
+static lv_style_t layer_style;
+
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen = lv_obj_create(NULL);
+
+    lv_style_init(&global_style);
+    lv_style_set_text_font(&global_style, &lv_font_unscii_8);
+    lv_style_set_text_letter_space(&global_style, 1);
+    lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
+
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_t *layer = zmk_widget_layer_status_obj(&layer_status_widget);
+    lv_style_init(&layer_style);
+    lv_style_set_text_font(&layer_style, &lv_font_montserrat_14);
+    lv_obj_add_style(layer, &layer_style, LV_PART_MAIN);
+    lv_obj_set_width(layer, 128);
+    lv_obj_align(layer, LV_ALIGN_TOP_MID, 0, -2);
+
+    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_LEFT_MID, 0, 2);
+
+#if IS_ENABLED(CONFIG_ZMK_BATTERY)
+    zmk_widget_dongle_battery_status_init(&battery_status_widget, screen);
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&battery_status_widget), LV_ALIGN_RIGHT_MID, 0, 2);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
+    lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+#endif
+
+    zmk_widget_modifiers_init(&modifiers_widget, screen);
+    lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+
+    return screen;
+}
